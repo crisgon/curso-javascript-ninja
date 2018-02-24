ab8bed2da586f1372cf7b491bb6726a5b76cfbde
crison <kristiano.gon@gmail.com>
2018-02-24 18:01:42 -0300

    Solved challenges: 5 and 6

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,23 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+
+let myArr = [1, true, Infinity, 'string', NaN, {}];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+
+
+function showArr (arr) {
+    return arr;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+
+showArr(myArr)[1];
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +26,27 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+
+function showOtherArr (arr, index) {
+    return arr[index];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+
+let newArr = [1,'2', true, undefined, NaN];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+
+showOtherArr(newArr, 0);
+showOtherArr(newArr, 1);
+showOtherArr(newArr, 2);
+showOtherArr(newArr, 3);
+showOtherArr(newArr, 4);
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +62,57 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+
+function bookList (bookName) {
+
+    let books = {
+        'Naruto': {
+            'quantidadePaginas':100,
+            'autor': 'Kishimoto',
+            'editora': 'Jump'
+        },
+
+        'Bleach': {
+            'quantidadePaginas':100,
+            'autor': 'Kubo',
+            'editora': 'Jump'
+        },
+        'One piece': {
+            'quantidadePaginas':100,
+            'autor': 'Odda',
+            'editora': 'Jump'
+        }
+    };
+
+    return bookName ? books[bookName] : books ;
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+
+bookList();
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+
+"O livro " + Object.keys(bookList())[0] + " tem " + bookList('Naruto').quantidadePaginas +" páginas!"
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+
+"O autor do livro " + Object.keys(bookList())[1] + " é " + bookList('Bleach').autor + "."
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+
+"O livro " + Object.keys(bookList())[2] + " foi publicado pela editora " + bookList('One piece').editora + "."
--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,7 +4,9 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-// ?
+
+let championship = 'Carioca';
+console.log(championship);
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -12,7 +14,8 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-// ?
+
+let teams = ['Flamengo', 'Botafogo', 'Vasco', 'Fluminense', 'Volta Redonda'];
 
 console.log( 'Times que estão participando do campeonato:', teams );
 
@@ -32,19 +35,35 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     - Se não houver time para a posição passada, deve retornar a mensagem:
     "Não temos a informação do time que está nessa posição."
 */
-// ?
+
+function showTeamPosition(num) {
+
+    if(num > teams.length)
+        return "Não temos a informação do time que está nessa posição.";
+
+    return `O time que está em ${num}º lugar é o ${teams[num - 1]}.`;
+}
 
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
 função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
 */
-// ?
+
+showTeamPosition(1); // 'O time que está em 1º lugar é o Flamengo.'
+showTeamPosition(2); // 'O time que está em 2º lugar é o Botafogo.'
+showTeamPosition(6); //'Não temos a informação do time que está nessa posição.'
+showTeamPosition(5); //'O time que está em 5º lugar é o Volta Redonda.'
+
 
 /*
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
-// ?
+
+let num = 20;
+while(num <= 30){
+    console.log(num++);
+}
 
 /*
 Crie uma função chamada `convertToHex`, com as seguintes características:
@@ -58,9 +77,39 @@ Crie uma função chamada `convertToHex`, com as seguintes características:
     a frase:
     "Não temos o equivalente hexadecimal para [COR]."
 */
-// ?
+
+function convertToHex(color) {
+    
+    switch(color){
+        case 'red':
+            return `O hexadecimal para a cor ${color} é #ff0000.`;
+
+        case 'green':
+            return `O hexadecimal para a cor ${color} é #008744.`;
+
+        case 'blue':
+            return `O hexadecimal para a cor ${color} é #0057e7.`;
+
+        case 'yellow':
+            return `O hexadecimal para a cor ${color} é #ffa700.`;
+        
+        case 'black':
+            return `O hexadecimal para a cor ${color} é #000000.`;
+
+        default:
+            return `Não temos o equivalente hexadecimal para ${color}.`;
+    };
+}
 
 /*
 Tente mostrar o hexadecimal de 8 cores diferentes usando a função criada acima.
 */
-// ?
+
+convertToHex('red'); //'O hexadecimal para a cor red é #ff0000.'
+convertToHex('green'); //'O hexadecimal para a cor green é #008744.'
+convertToHex('blue'); //'O hexadecimal para a cor blue é #0057e7.'
+convertToHex('black'); //'O hexadecimal para a cor black é #000000.'
+convertToHex('yellow'); //'O hexadecimal para a cor yellow é #ffa700.'
+convertToHex('white'); //'Não temos o equivalente hexadecimal para white.'
+convertToHex('pink'); //'Não temos o equivalente hexadecimal para pink.'
+convertToHex('gray'); //'Não temos o equivalente hexadecimal para gray.'
