ea6423de62916ec9f5736cafcaeac91bcadc714e
crison <kristiano.gon@gmail.com>
2018-02-25 23:32:34 -0300

    Solved challenges: 7 and 8

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -1,51 +1,79 @@
 /*
 Crie um array com 5 items (tipos variados).
 */
-// ?
+
+let myArr = [1,'dois', true, NaN, [1,2,3]]
 
 /*
 Crie uma função chamada `addItem`, que irá adicionar itens no array criado.
 A função deverá retornar o array atualizado.
 */
-// ?
+
+let addItem = function addItem(item, arr) {
+  return arr.push(item);
+}
 
 /*
 Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
-// ?
+
+addItem([4,5,6], myArr);
+console.log(myArr);
 
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
 frase:
 "O segundo elemento do segundo array é [ELEMENTO]."
 */
-// ?
+
+console.log(`O segundo elemento do segundo array é ${myArr[5][1]}.`);
 
 /*
 Mostre no console quantos itens tem o primeiro array criado, com a frase:
 "O primeiro array tem [QUANTIDADE DE ITENS] itens."
 */
-// ?
+
+console.log(`O primeiro array tem ${myArr.length} itens.`);
 
 /*
 Agora mostre no console quantos itens tem o segundo array criado, com a frase:
 "O segundo array tem [QUANTIDADE DE ITENS] itens."
 */
-// ?
+
+console.log(`O segundo array tem ${myArr[5].length} itens.`);
 
 /*
 Utilizando a estrutura de repetição `while`, mostre no console todos os números
 pares entre 10 e 20, inclusive esses 2.
 */
+
 console.log( 'Números pares entre 10 e 20:' );
-// ?
+
+let cont = 10;
+
+while(cont <= 20){
+
+	if (cont%2 === 0) {
+		console.log(cont);
+	}
+  cont++;
+}
 
 /*
 Na mesma ideia do exercício acima: mostre agora os números ímpares.
 */
 console.log( 'Números ímpares entre 10 e 20:' );
-// ?
+
+let newCont = 10;
+
+while(newCont <= 20){
+	
+	if (newCont%2 !== 0) {
+		console.log(newCont);
+	}
+  newCont++;
+}
 
 /*
 Repita os mesmos exercícios feitos acima, mas agora usando o loop "for".
@@ -54,7 +82,13 @@ Só vamos mudar o range:
 - No segundo "for", mostre os números ímpares entre 111 e 125, inclusive eles.
 */
 console.log( 'Números pares entre 100 e 120:' );
-// ?
+
+for(let cont = 100; cont <= 120; cont += 2){
+	console.log(cont);
+}
 
 console.log( 'Números ímpares entre 111 e 125:' );
-// ?
+
+for(let cont = 111; cont <= 125; cont += 2){
+	console.log(cont);
+}
--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -3,38 +3,49 @@ Declare uma variável chamada `sum` e atribua a ela uma função chamada
 `calculateSum`. A função deve receber dois parâmetros e retornar a soma
 desses parâmetros.
 */
-// ?
+
+let sumfunc = function sumfunc (a, b) {
+  return a + b;
+};
 
 /*
 Invoque a função criada acima, passando dois números que serão somados, e mostre
 o resultado no console, com a frase:
 "A soma de [VALOR 1] e [VALOR2] é igual a [RESULTADO]."
 */
-// ?
+
+console.log(`A soma de 2 e 4 é igual a ${sumfunc(2,4)}.`);
 
 /*
 Mostre no console o nome da função criada acima, com a frase:
 "O nome da função que faz a soma é [NOME DA FUNÇÃO]."
 */
-// ?
+
+console.log(`O nome da função que faz a soma é ${sumfunc.name}.`);
 
 /*
 Crie uma função literal chamada `showName`. Essa função deve retornar o
 seu nome.
 */
-// ?
+
+function showName() {
+  return 'Cristiano Gonçalves';
+}
 
 /*
 Declare uma variável chamada `varShowName` que recebe a função criada acima.
 */
-// ?
+
+let varShowName = showName;
 
 /*
 Usando a variável criada acima, mostre no console o nome e o retorno da função
 atribuída a ela, com a seguinte frase:
 "A função [NOME DA FUNÇÃO] retorna [RETORNO DA FUNÇÃO]."
 */
-// ?
+
+console.log(`A função ${varShowName.name} retorna ${varShowName()}.`);
+
 
 /*
 Crie uma função literal chamada `calculator`, que funcione assim:
@@ -48,28 +59,103 @@ vai efetuar. Será uma string com os valores `+`, `-`, `*`, `/` ou `%`;
   - Se o operador não for válido, retornar a frase:
   "Operação inválida."
 */
-// ?
+
+function calculator(operator) {
+  if(operator === '+') 
+    return function sum (a, b) {
+      return `Resultado da operação: ${a} ${operator} ${b} = ${a + b}.`;
+  };
+  
+  if(operator === '-')
+    return function sub (a, b) {
+      return `Resultado da operação: ${a} ${operator} ${b} = ${a - b}.`;
+  };
+
+  if(operator === '*')
+    return function mult (a, b) {
+      return `Resultado da operação: ${a} ${operator} ${b} = ${a * b}.`;
+  };
+
+  if(operator === '/')
+    return function div (a, b) {
+      return `Resultado da operação: ${a} ${operator} ${b} = ${a / b}.`;
+  };
+
+  if(operator === '%')
+    return function mod (a, b) {
+      return `Resultado da operação: ${a} ${operator} ${b} = ${a % b}.`;
+  };
+
+  return 'Operação inválida';
+}
+
+
+// Opção otimizada utilizando switch
+
+function otherCalculator(operator) {
+  
+  return function(a, b) {
+    let result;
+
+    switch( operator ) {
+      case '+':
+        result =  a + b;
+        break;
+      case '-':
+        result = a - b;
+        break;
+      case '*':
+        result = a * b;
+        break;
+      case '/':
+        result = a / b;
+        break;
+      case '%':
+        result = a % b;
+        break;
+      default:
+        return 'Operação inválida';
+    }
+
+    return `Resultado da operação: ${a} ${operator} ${b} = ${result}.`;
+  };  
+}
 
 /*
 Declare uma variável chamada `sum`, que receberá a função acima, passando como
 parâmetro o operador de soma.
 */
-// ?
+
+let sum = calculator('+');
 
 /*
 Agora `sum` é uma função. Mostre no console a soma de dois números, usando ela.
 */
-// ?
+
+console.log(sum(3,5));
 
 /*
 Agora, declare algumas variáveis com os nomes `subtraction`, `multiplication`,
 `division` e `mod`, e atribua a elas a função `calculator`, passando o operador
 correto por parâmetro para cada uma delas.
 */
-// ?
+
+let subtraction = calculator('-');
+let multiplication = calculator('*');
+let division = calculator('/');
+let mod = calculator('%');
 
 /*
 Faça uma operação com cada uma das funções criadas acima, mostrando o resultado
 no console.
 */
-// ?
+
+console.log(subtraction(5,4));
+
+console.log(multiplication(3,4));
+
+console.log(division(10,2));
+
+console.log(mod(7,2));
+
+console.log(otherCalculator('+')(30,40));
